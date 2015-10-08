e2e273dc3ab2d731bc034ecde967ab38b54244db
quentin <quentin.reynaud@master-stic.fr>
2015-10-08 23:15:35 +0200

    ajout fonction carrer

--- code_sensor_detector.c
+++ code_sensor_detector.c
@@ -1,12 +1,16 @@
 #include <stdioh>
 #include <stdint>
 
+int fct_carrer(int nbr){
+  return nbr*nbr;
+}
 int main (void){
   uint8_t tab_entier [10] ={1,2,3,4,5,6,7,8,9,10};
-  uint8_t index = 0;
+  uint8_t index = 0,a = 5;
   for (index=0;index<10;index++){
     printf("%d\n",tab_entier[index]);
   }
-  printf("hello world!!\n");
+  a = fct_carrer(a);
+  printf("hello world!! = %d\n",a);
   return 0;
 }
