83497a024053f30fea16f35f33e1376c7ad3ce40
reyq <quentin.reynaud@master-stic.fr>
2015-10-08 22:56:48 +0200

    ajout fonction

--- code_sensor_detector.c
+++ code_sensor_detector.c
@@ -1,7 +1,12 @@
 #include <stdioh>
+#include <stdint>
 
 int main (void){
+  uint8_t tab_entier [10] ={1,2,3,4,5,6,7,8,9,10};
+  uint8_t index = 0;
+  for index=0;index<10;index++){
+    printf("%d\n",tab_entier[index]);
+  }
   printf("hello world!!\n");
-  while(1),
   return 0;
 }
