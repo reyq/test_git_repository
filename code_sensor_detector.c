3cf9a9c5f90e26a8276c55015c45c56c85eff026
quentin <quentin.reynaud@master-stic.fr>
2015-10-08 23:00:33 +0200

    correction programme principal

--- code_sensor_detector.c
+++ code_sensor_detector.c
@@ -4,7 +4,7 @@
 int main (void){
   uint8_t tab_entier [10] ={1,2,3,4,5,6,7,8,9,10};
   uint8_t index = 0;
-  for index=0;index<10;index++){
+  for (index=0;index<10;index++){
     printf("%d\n",tab_entier[index]);
   }
   printf("hello world!!\n");
